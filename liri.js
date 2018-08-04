6728b31ad3e819133ebb2ccdc463688c454da5ab
Adam Bilsky <adambilsky@gmail.com>
2018-08-04 15:50:12 -0500

    finished all 4 commands

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-
+var request = require("request");
 // Step 8
 require('dotenv').config();
 var fs = require("fs");
@@ -20,11 +20,16 @@ var command = process.argv[2];
 var term = process.argv.slice(3).join(" ");
 
 function myTweets() {
-
+    var params = { screen_name: 'boot_sky' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+        // need to parse tweets to get JUST text
+    });
 }
 
 function spotifyThis() {
-    spotifyQuery = process.argv[3]
     spotify.search({
         type: 'track',
         query: term
@@ -34,7 +39,7 @@ function spotifyThis() {
                 return console.log('Error occurred: ' + err);
             }
             // console.log(JSON.stringify(data, null, 2));
-            console.log("Song name: ", spotifyQuery);
+            console.log("Song name: ", term);
             console.log("Artist: ", data.tracks.items[3].artists[0].name);
             console.log("Link: ", data.tracks.items[3].preview_url);
             console.log("Album: ", data.tracks.items[3].name);
@@ -44,19 +49,26 @@ function spotifyThis() {
 }
 
 function movieThis() {
+    var request = require("request");
+    request("http://www.omdbapi.com/?t=" + term + "&apikey=trilogy", function (error, response, body) {
 
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+            console.log(body);
+        }
+    })
 }
 
 function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function (error, data) {
-
         // If the code experiences any errors it will log the error to the console.
         if (error) {
             return console.log(error);
         }
         console.log(data);
-        var dataArr = data.split(",");
-        spotifyQuery = dataArr[1];
+        var randomArray = data.split(",");
+        term = randomArray[1];
+        console.log(term);
         spotifyThis();
     })
 }
